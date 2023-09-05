a7df0ad3e0fb57b0b6c09058fa6bfa7e99beb8f1
Franklin Menezes <franklin.dev.full@gmail.com>
2023-09-05 14:48:56 -0300

    pratica de strings

---
+++ manipString.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string nome;
+
+    cout << "Digite seu nome: ";
+    // getline(cin, nome);
+    cin  >> nome;
+    cout << endl << "Seu nome eh " << nome;
+}
---
+++ stringC.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// using namespace std;
+
+int main() {
+    char nome[10] = "Oi Mundo";
+    char x;
+    nome[10] = 'G';
+    std::cout << x;
+}
